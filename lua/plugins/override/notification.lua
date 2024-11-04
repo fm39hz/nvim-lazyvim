31e05ff3de3eff8a0f79005495d279ddbecc7f8d
fm39hz-laptop <hitpoint2k3@gmail.com>
2024-11-04 09:06:04 +0700

    refactor: splits visualize to smaller files

---
+++ lua/plugins/override/notification.lua
@@ -0,0 +1,24 @@
+return {
+	{
+		"folke/noice.nvim",
+		opts = {
+			presets = {
+				bottom_search = false,
+				lsp_doc_border = true,
+			},
+			lsp = {
+				hover = {
+					silent = true,
+				},
+			},
+		},
+	},
+	{
+		"rcarriga/nvim-notify",
+		opts = {
+			timeout = 2500,
+			fps = 170,
+			stages = "fade_in_slide_out",
+		},
+	},
+}
---
+++ lua/plugins/override/tabline.lua
@@ -0,0 +1,18 @@
+return {
+  {
+    "akinsho/bufferline.nvim",
+    opts = {
+      options = {
+        separator_style = "slope",
+        hover = {
+          enabled = true,
+          delay = 200,
+          reveal = { "close" },
+        },
+        indicator = {
+          style = "underline",
+        },
+      },
+    },
+  },
+}
