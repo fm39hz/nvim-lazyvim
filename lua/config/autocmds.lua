b2b8915ff65ea2a73061d8b822237ee9b619f240
fm39hz-laptop <hitpoint2k3@gmail.com>
2024-12-24 16:26:28 +0700

    feat: disable copilot on suggestion

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -50,3 +50,18 @@ vim.api.nvim_create_autocmd({ "VimEnter", "VimLeave" }, {
 		end
 	end,
 })
+
+vim.api.nvim_create_autocmd("User", {
+	pattern = "BlinkCmpCompletionMenuOpen",
+	callback = function()
+		require("copilot.suggestion").dismiss()
+		vim.b.copilot_suggestion_hidden = true
+	end,
+})
+
+vim.api.nvim_create_autocmd("User", {
+	pattern = "BlinkCmpCompletionMenuClose",
+	callback = function()
+		vim.b.copilot_suggestion_hidden = false
+	end,
+})
