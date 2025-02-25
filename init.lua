50057ac30f1efe110eb91e31f7b6a511ad322159
fm39hz-laptop <hitpoint2k3@gmail.com>
2025-02-25 10:31:39 +0700

    style(neovide): adjust font size and linespace
    chore(init): remove unnecessary lines

--- init.lua
+++ init.lua
@@ -1,8 +1,7 @@
 require("config.lazy")
 vim.cmd("colorscheme ex-everforest")
 if vim.g.neovide then
-	vim.o.guifont = "JetBrains Mono:h15.4:b"
-	vim.opt.linespace = -4
+	vim.o.guifont = "JetBrains Mono:h15:b"
 	vim.g.neovide_refresh_rate = 170
 end
 vim.o.sessionoptions = "blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions"
