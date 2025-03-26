0f359d5d8318bc4798a1ec2d969283e8e609ea67
FM39hz <hitpoint2k3@gmail.com>
2025-03-26 20:11:14 +0700

    refactor(autocmds): remove duplicate LspAttach autocmd and Mini.files rounded border

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -2,15 +2,6 @@
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
 
--- Set Mini.files rounded border
-vim.api.nvim_create_autocmd("User", {
-	pattern = "MiniFilesWindowOpen",
-	callback = function(args)
-		local win_id = args.data.win_id
-		vim.api.nvim_win_set_config(win_id, { border = "rounded" })
-	end,
-})
-
 vim.api.nvim_create_autocmd("LspAttach", {
 	callback = function(args)
 		local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
@@ -63,22 +54,5 @@ vim.api.nvim_create_autocmd("User", {
 	pattern = "BlinkCmpCompletionMenuClose",
 	callback = function()
 		vim.b.copilot_suggestion_hidden = false
-	end,
-})
-
-vim.api.nvim_create_autocmd("LspAttach", {
-	callback = function(args)
-		local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
-		vim.api.nvim_create_autocmd("BufWritePre", {
-			group = augroup,
-			buffer = args.buf,
-			callback = function()
-				vim.lsp.buf.format({ timeout = 1000, async = false })
-
-				if vim.bo[0].filetype == "cs" then
-					require("csharp").fix_usings()
-				end
-			end,
-		})
 	end,
 })
