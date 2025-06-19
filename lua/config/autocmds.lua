4ee5e99ef53446c0acb9a45c213e255b60db8ee5
fm39hz-laptop <hitpoint2k3@gmail.com>
2025-06-19 21:37:15 +0700

    chore: Remove IBus integration and Copilot menu autocmds

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -42,73 +42,17 @@ vim.api.nvim_create_autocmd({ "VimEnter", "VimLeave" }, {
 	end,
 })
 
-vim.api.nvim_create_autocmd("User", {
-	pattern = "BlinkCmpCompletionMenuOpen",
-	callback = function()
-		require("copilot.suggestion").dismiss()
-		vim.b.copilot_suggestion_hidden = true
-	end,
-})
-
-vim.api.nvim_create_autocmd("User", {
-	pattern = "BlinkCmpCompletionMenuClose",
-	callback = function()
-		vim.b.copilot_suggestion_hidden = false
-	end,
-})
-local ibus_prev_engine = nil
-
-function IBusOff()
-	-- Lưu engine hiện tại
-	ibus_prev_engine = vim.fn.system("ibus engine")
-	-- Chuyển sang engine tiếng Anh
-	vim.cmd("silent !ibus engine xkb:us::eng")
-end
-
-function IBusOn()
-	local current_engine = vim.fn.system("ibus engine")
-	-- Nếu engine được set là tiếng Anh thì giữ nguyên,
-	-- Nếu là Bamboo thì chuyển về lại Bamboo
-	if not string.match(current_engine, "xkb:us::eng") then
-		ibus_prev_engine = current_engine
-	end
-	-- Khôi phục lại engine
-	vim.cmd("silent !ibus engine " .. ibus_prev_engine)
-end
-
-vim.api.nvim_create_augroup("IBusHandler", { clear = true })
-
-vim.api.nvim_create_autocmd({ "CmdlineEnter" }, {
-	pattern = "[/?]",
-	callback = function()
-		IBusOn()
-	end,
-	group = "IBusHandler",
-})
-
-vim.api.nvim_create_autocmd({ "CmdlineLeave" }, {
-	pattern = "[/?]",
-	callback = function()
-		IBusOff()
-	end,
-	group = "IBusHandler",
-})
-
-vim.api.nvim_create_autocmd({ "InsertEnter" }, {
-	pattern = "*",
-	callback = function()
-		IBusOn()
-	end,
-	group = "IBusHandler",
-})
-
-vim.api.nvim_create_autocmd({ "InsertLeave" }, {
-	pattern = "*",
-	callback = function()
-		IBusOff()
-	end,
-	group = "IBusHandler",
-})
-
--- Call IBusOff initially
-IBusOff()
+-- vim.api.nvim_create_autocmd("User", {
+-- 	pattern = "BlinkCmpCompletionMenuOpen",
+-- 	callback = function()
+-- 		require("copilot.suggestion").dismiss()
+-- 		vim.b.copilot_suggestion_hidden = true
+-- 	end,
+-- })
+--
+-- vim.api.nvim_create_autocmd("User", {
+-- 	pattern = "BlinkCmpCompletionMenuClose",
+-- 	callback = function()
+-- 		vim.b.copilot_suggestion_hidden = false
+-- 	end,
+-- })
