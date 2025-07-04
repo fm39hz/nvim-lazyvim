ab1d024a3a54867354ed41371780bffb045baf85
fm39hz-laptop <hitpoint2k3@gmail.com>
2025-07-04 23:35:28 +0700

    fix(dap): adjust layout sizes for better UI

--- lua/plugins/override/development.lua
+++ lua/plugins/override/development.lua
@@ -137,6 +137,7 @@ return {
 				menu = { border = "rounded" },
 				documentation = { window = { border = "rounded" } },
 				trigger = {
+					show_in_snippet = false,
 					show_on_insert_on_trigger_character = false,
 				},
 			},
@@ -223,17 +224,17 @@ return {
 			layouts = {
 				{
 					elements = {
-						{ id = "watches",     size = 0.24 },
-						{ id = "scopes",      size = 0.24 },
+						{ id = "watches", size = 0.24 },
+						{ id = "scopes", size = 0.24 },
 						{ id = "breakpoints", size = 0.24 },
-						{ id = "stacks",      size = 0.28 },
+						{ id = "stacks", size = 0.28 },
 					},
 					size = 0.23,
 					position = "right",
 				},
 				{
 					elements = {
-						{ id = "repl",    size = 0.55 },
+						{ id = "repl", size = 0.55 },
 						{ id = "console", size = 0.45 },
 					},
 					size = 0.27,
