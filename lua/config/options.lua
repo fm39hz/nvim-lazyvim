a25eefcd9c0adefdfa2fe6683a7dc4929df10663
fm39hz-laptop <hitpoint2k3@gmail.com>
2025-08-01 09:52:11 +0700

    feat: disable LSP logging

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -12,3 +12,6 @@ vim.o.foldcolumn = "1" -- '0' is not bad
 vim.o.foldlevel = 99   -- Using ufo provider need a large value, feel free to decrease the value
 vim.o.foldlevelstart = 99
 vim.o.foldenable = true
+
+-- lsp config
+vim.lsp.set_log_level("off")
