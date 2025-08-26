868e51134cf5a2f856b24d3b7fcfa9f361dfd935
fm39hz <hitpoint2k3@gmail.com>
2025-08-26 16:30:46 +0700

    chore: remove unused indent-o-matic plugin

--- lua/plugins/override/snacks.lua
+++ lua/plugins/override/snacks.lua
@@ -1,80 +1,81 @@
 local function createLogo(withBorder)
-	local lines = {
-		"                                ⣴⣶⣤⡤⠦⣤⣀⣤⠆     ⣈⣭⣿⣶⣿⣦⣼⣆                                  ",
-		"                                 ⠉⠻⢿⣿⠿⣿⣿⣶⣦⠤⠄⡠⢾⣿⣿⡿⠋⠉⠉⠻⣿⣿⡛⣦                               ",
-		"                                       ⠈⢿⣿⣟⠦ ⣾⣿⣿⣷    ⠻⠿⢿⣿⣧⣄                             ",
-		"                                        ⣸⣿⣿⢧ ⢻⠻⣿⣿⣷⣄⣀⠄⠢⣀⡀⠈⠙⠿⠄                            ",
-		"                                       ⢠⣿⣿⣿⠈    ⣻⣿⣿⣿⣿⣿⣿⣿⣛⣳⣤⣀⣀                           ",
-		"                                ⢠⣧⣶⣥⡤⢄ ⣸⣿⣿⠘  ⢀⣴⣿⣿⡿⠛⣿⣿⣧⠈⢿⠿⠟⠛⠻⠿⠄                          ",
-		"   ███▄    █ ▓█████  ▒█████    ⣰⣿⣿⠛⠻⣿⣿⡦⢹⣿⣷   ⢊⣿⣿⡏  ⢸⣿⣿⡇ ⢀⣠⣄⣾⠄    ██▒   █▓ ██▓ ███▄ ▄███▓",
-		"   ██ ▀█   █ ▓█   ▀ ▒██▒  ██▒ ⣠⣿⠿⠛ ⢀⣿⣿⣷⠘⢿⣿⣦⡀ ⢸⢿⣿⣿⣄ ⣸⣿⣿⡇⣪⣿⡿⠿⣿⣷⡄  ▓██░   █▒▓██▒▓██▒▀█▀ ██▒",
-		"  ▓██  ▀█ ██▒▒███   ▒██░  ██▒ ⠙⠃   ⣼⣿⡟  ⠈⠻⣿⣿⣦⣌⡇⠻⣿⣿⣷⣿⣿⣿ ⣿⣿⡇ ⠛⠻⢷⣄  ▓██  █▒░▒██▒▓██    ▓██░",
-		"  ▓██▒  ▐▌██▒▒▓█  ▄ ▒██   ██░      ⢻⣿⣿⣄   ⠈⠻⣿⣿⣿⣷⣿⣿⣿⣿⣿⡟ ⠫⢿⣿⡆       ▒██ █░░░██░▒██    ▒██ ",
-		"  ▒██░   ▓██░░▒████▒░ ████▓▒░       ⠻⣿⣿⣿⣿⣶⣶⣾⣿⣿⣿⣿⣿⣿⣿⣿⡟⢀⣀⣤⣾⡿⠃        ▒▀█░  ░██░▒██▒   ░██▒",
-		"   ▒░   ▒ ▒ ░░ ▒░ ░░ ▒░▒░▒░    ╰──────────────────────────────╯    ░ ▐░  ░▓  ░ ▒░   ░  ░",
-		"   ░░   ░ ▒░ ░ ░  ░  ░ ▒ ▒░                                        ░ ░░   ▒ ░░  ░      ░",
-		"    ░   ░ ░    ░   ░ ░ ░ ▒                                           ░░   ▒ ░░      ░   ",
-		"          ░    ░  ░    ░ ░                                            ░   ░         ░   ",
-		"                                                                     ░                  ",
-	}
+  local lines = {
+    "                                ⣴⣶⣤⡤⠦⣤⣀⣤⠆     ⣈⣭⣿⣶⣿⣦⣼⣆                                  ",
+    "                                 ⠉⠻⢿⣿⠿⣿⣿⣶⣦⠤⠄⡠⢾⣿⣿⡿⠋⠉⠉⠻⣿⣿⡛⣦                               ",
+    "                                       ⠈⢿⣿⣟⠦ ⣾⣿⣿⣷    ⠻⠿⢿⣿⣧⣄                             ",
+    "                                        ⣸⣿⣿⢧ ⢻⠻⣿⣿⣷⣄⣀⠄⠢⣀⡀⠈⠙⠿⠄                            ",
+    "                                       ⢠⣿⣿⣿⠈    ⣻⣿⣿⣿⣿⣿⣿⣿⣛⣳⣤⣀⣀                           ",
+    "                                ⢠⣧⣶⣥⡤⢄ ⣸⣿⣿⠘  ⢀⣴⣿⣿⡿⠛⣿⣿⣧⠈⢿⠿⠟⠛⠻⠿⠄                          ",
+    "   ███▄    █ ▓█████  ▒█████    ⣰⣿⣿⠛⠻⣿⣿⡦⢹⣿⣷   ⢊⣿⣿⡏  ⢸⣿⣿⡇ ⢀⣠⣄⣾⠄    ██▒   █▓ ██▓ ███▄ ▄███▓",
+    "   ██ ▀█   █ ▓█   ▀ ▒██▒  ██▒ ⣠⣿⠿⠛ ⢀⣿⣿⣷⠘⢿⣿⣦⡀ ⢸⢿⣿⣿⣄ ⣸⣿⣿⡇⣪⣿⡿⠿⣿⣷⡄  ▓██░   █▒▓██▒▓██▒▀█▀ ██▒",
+    "  ▓██  ▀█ ██▒▒███   ▒██░  ██▒ ⠙⠃   ⣼⣿⡟  ⠈⠻⣿⣿⣦⣌⡇⠻⣿⣿⣷⣿⣿⣿ ⣿⣿⡇ ⠛⠻⢷⣄  ▓██  █▒░▒██▒▓██    ▓██░",
+    "  ▓██▒  ▐▌██▒▒▓█  ▄ ▒██   ██░      ⢻⣿⣿⣄   ⠈⠻⣿⣿⣿⣷⣿⣿⣿⣿⣿⡟ ⠫⢿⣿⡆       ▒██ █░░░██░▒██    ▒██ ",
+    "  ▒██░   ▓██░░▒████▒░ ████▓▒░       ⠻⣿⣿⣿⣿⣶⣶⣾⣿⣿⣿⣿⣿⣿⣿⣿⡟⢀⣀⣤⣾⡿⠃        ▒▀█░  ░██░▒██▒   ░██▒",
+    "   ▒░   ▒ ▒ ░░ ▒░ ░░ ▒░▒░▒░    ╰──────────────────────────────╯    ░ ▐░  ░▓  ░ ▒░   ░  ░",
+    "   ░░   ░ ▒░ ░ ░  ░  ░ ▒ ▒░                                        ░ ░░   ▒ ░░  ░      ░",
+    "    ░   ░ ░    ░   ░ ░ ░ ▒                                           ░░   ▒ ░░      ░   ",
+    "          ░    ░  ░    ░ ░                                            ░   ░         ░   ",
+    "                                                                     ░                  ",
+  }
 
-	if withBorder then
-		local result =
-		"╭─────────────────────────────────────────────────────────────────────────────────────────╮\n"
-		for _, line in ipairs(lines) do
-			result = result .. "│" .. line .. "│\n"
-		end
-		result = result
-				.. "╰─────────────────────────────────────────────────────────────────────────────────────────╯"
-		return result
-	else
-		return table.concat(lines, "\n")
-	end
+  if withBorder then
+    local result =
+    "╭─────────────────────────────────────────────────────────────────────────────────────────╮\n"
+    for _, line in ipairs(lines) do
+      result = result .. "│" .. line .. "│\n"
+    end
+    result = result
+        .. "╰─────────────────────────────────────────────────────────────────────────────────────────╯"
+    return result
+  else
+    return table.concat(lines, "\n")
+  end
 end
 
 local logo = createLogo(false)
 return {
-	{
-		"folke/snacks.nvim",
-		opts = {
-			image = {
-				force = true,
-			},
-			quickfile = {},
-			scroll = {},
-			statuscolumn = {},
-			words = {},
-			lazygit = {
-				win = {
-					style = "lazygit",
-					border = "none",
-					width = 130,
-					height = 32,
-					row = 1,
-				},
-			},
-			dashboard = {
-				preset = {
-					header = logo,
-				},
-				sections = {
-					{ section = "header" },
-					{ icon = " ", title = "Keymaps", section = "keys", indent = 2, padding = 1 },
-					{ section = "startup" },
-				},
-			},
-			terminal = {
-				win = {
-					style = "terminal",
-					position = "float",
-					border = "rounded",
-					width = 128,
-					height = 16,
-					row = 1,
-					title_pos = "top",
-				},
-				interactive = true,
-			},
-		},
-	},
+  {
+    "folke/snacks.nvim",
+    opts = {
+      image = {
+        force = true,
+      },
+      quickfile = {},
+      scroll = {},
+      statuscolumn = {},
+      words = {},
+      input = {},
+      lazygit = {
+        win = {
+          style = "lazygit",
+          border = "none",
+          width = 130,
+          height = 32,
+          row = 2,
+        },
+      },
+      dashboard = {
+        preset = {
+          header = logo,
+        },
+        sections = {
+          { section = "header" },
+          { icon = " ", title = "Keymaps", section = "keys", indent = 2, padding = 1 },
+          { section = "startup" },
+        },
+      },
+      terminal = {
+        win = {
+          style = "terminal",
+          position = "float",
+          border = "rounded",
+          width = 128,
+          height = 16,
+          row = 1,
+          title_pos = "top",
+        },
+        interactive = true,
+      },
+    },
+  },
 }
