3c931bca837bab6df1c4ed1c55694bfeeb19568d
fm39hz <hitpoint2k3@gmail.com>
2026-06-14 12:53:21 +0700

    feat(font): configure Iosevka for neovide

--- init.lua
+++ init.lua
@@ -4,7 +4,7 @@ end
 require("config.lazy")
 vim.cmd("colorscheme ex-everforest")
 if vim.g.neovide then
-	vim.o.guifont = "JetBrains Mono:h15:b"
+	vim.o.guifont = "Iosevka Sans:h15:b"
 	vim.g.neovide_refresh_rate = 170
 end
 vim.o.sessionoptions = "blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions"
