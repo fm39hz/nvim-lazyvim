941827100dac405d82bd50bd435fdaf5e85f9276
fm39hz <hitpoint2k3@gmail.com>
2026-06-19 13:19:26 +0700

    feat(ai): add opencode.nvim

---
+++ lua/plugins/development/ai.lua
@@ -0,0 +1,19 @@
+return {
+	-- {
+	-- 	"sudo-tee/opencode.nvim",
+	-- 	event = "BufReadPost",
+	-- 	dependencies = {
+	-- 		"nvim-lua/plenary.nvim",
+	-- 		{
+	-- 			"MeanderingProgrammer/render-markdown.nvim",
+	-- 			opts = {
+	-- 				anti_conceal = { enabled = false },
+	-- 				file_types = { "markdown", "opencode_output" },
+	-- 			},
+	-- 			ft = { "markdown", "Avante", "copilot-chat", "opencode_output" },
+	-- 		},
+	-- 		"saghen/blink.cmp",
+	-- 		"folke/snacks.nvim",
+	-- 	},
+	-- },
+}
