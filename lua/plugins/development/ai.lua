6bd47efbf455496f21ac53789f99f59d2bb98d54
fm39hz <hitpoint2k3@gmail.com>
2026-07-17 14:15:54 +0700

    feat(ai): remove opencode.nvim

--- lua/plugins/development/ai.lua
+++ lua/plugins/development/ai.lua
@@ -1,19 +1,9 @@
 return {
-	-- {
-	-- 	"sudo-tee/opencode.nvim",
-	-- 	event = "BufReadPost",
-	-- 	dependencies = {
-	-- 		"nvim-lua/plenary.nvim",
-	-- 		{
-	-- 			"MeanderingProgrammer/render-markdown.nvim",
-	-- 			opts = {
-	-- 				anti_conceal = { enabled = false },
-	-- 				file_types = { "markdown", "opencode_output" },
-	-- 			},
-	-- 			ft = { "markdown", "Avante", "copilot-chat", "opencode_output" },
-	-- 		},
-	-- 		"saghen/blink.cmp",
-	-- 		"folke/snacks.nvim",
-	-- 	},
-	-- },
+	{
+		"HesamKorki/pi-ide",
+		keys = {
+			{ "<leader>aw", "<cmd>AgentWorkspace<cr>", desc = "Open Pi-Agent Workspace" },
+		},
+		opts = {},
+	},
 }
