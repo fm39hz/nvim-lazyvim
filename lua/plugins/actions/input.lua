31facce71151251fea591f6698d569e3a95ae510
fm39hz <fm39hz@gmail.com>
2026-07-27 09:53:56 +0700

    refactor(action): move action into right order

--- lua/plugins/actions/input.lua
+++ lua/plugins/actions/input.lua
@@ -1,11 +1,29 @@
 return {
-  {
-    "keaising/im-select.nvim",
-    event = "BufReadPost",
-    opts = {
-      default_command = "fcitx5-remote",
-      default_im_select = "keyboard-us",
-      set_default_events = { "VimEnter", "FocusGained", "InsertLeave", "CmdlineLeave" },
-    },
-  },
+	{
+		"keaising/im-select.nvim",
+		event = "BufReadPost",
+		opts = {
+			default_command = "fcitx5-remote",
+			default_im_select = "keyboard-us",
+			set_default_events = { "VimEnter", "FocusGained", "InsertLeave", "CmdlineLeave" },
+		},
+	},
+	{
+		"rachartier/tiny-code-action.nvim",
+		event = "BufReadPost",
+		dependencies = {
+			{ "folke/snacks.nvim" },
+		},
+		opts = {},
+		keys = {
+			{
+				"<leader>ca",
+				function()
+					require("tiny-code-action").code_action({})
+				end,
+				mode = { "n", "x" },
+				desc = "Code Action",
+			},
+		},
+	},
 }
