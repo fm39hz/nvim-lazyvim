595d73901274d57ea970e93b5cd14cf775f0e8bc
fm39hz <fm39hz@gmail.com>
2026-08-12 11:33:56 +0700

    feat(nvim): update packages into new nvim

--- lua/plugins/actions/ingest.lua
+++ lua/plugins/actions/ingest.lua
@@ -1,10 +1,10 @@
 return {
-  {
-    "0xrusowsky/nvim-ctx-ingest",
-    cmd = "CtxIngest",
-    dependencies = {
-      "nvim-web-devicons",
-    },
-    config = {},
-  },
+	{
+		"0xrusowsky/nvim-ctx-ingest",
+		cmd = "CtxIngest",
+		dependencies = {
+			"nvim-tree/nvim-web-devicons",
+		},
+		config = {},
+	},
 }
